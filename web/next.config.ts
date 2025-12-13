747f140b3416f2cd607d32537eb706d4832cbfd4
veski4a <veskoveskov2006@gmail.com>
2025-12-13 19:22:52 +0200

    Configure Docker and Next.js for improved development experience; enforce Webpack polling and adjust image handling

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // 1. Allow images from Django
   images: {
     remotePatterns: [
       {
@@ -10,6 +11,15 @@ const nextConfig: NextConfig = {
         pathname: '/media/**',
       },
     ],
+  },
+
+  // 2. Force Polling for Docker on Windows
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: 1000,   // Check for changes every second
+      aggregateTimeout: 300,
+    };
+    return config;
   },
 };
 
